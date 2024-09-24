622c4342a58238fd610da09323d448e9e1a228ed
justin-0 <itsjustinoconnell@gmail.com>
2024-09-24 16:25:25 +0100

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Ai notes app</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="outline outline-red-400 max-w-[700px] space-y-2">
+        <h1 className="text-5xl font-bold">
+          Keep track of your mood with your daily journal
+        </h1>
+        <p className="text-2xl">
+          Start writing your daily entires and our LLM will analyse it to let
+          you know how you are feeling
+        </p>
+        <div className="mt-4">
+          <Link href="/">
+            <button className="border border-black px-3 py-1 rounded-lg hover:bg-slate-100  transition">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
