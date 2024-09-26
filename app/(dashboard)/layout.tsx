9fa955ee5417ced88c9e2dbb31f2792d2ab51381
justin-0 <itsjustinoconnell@gmail.com>
2024-09-26 15:49:14 +0100

    header with clerk user button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardLayout({
   children,
 }: {
@@ -8,7 +10,14 @@ export default function DashboardLayout({
       <aside className="h-full fixed w-[180px] top-0 left-0 bottm-0 border-r border-black/20">
         Mood Notes
       </aside>
-      <div className="ml-[180px]">{children}</div>
+      <div className="ml-[180px] flex flex-col">
+        <header className="h-16 px-6 flex items-center justify-end border-b border-black/20">
+          <div>
+            <UserButton />
+          </div>
+        </header>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
