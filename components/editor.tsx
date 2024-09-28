22c83f2dbf4f7201bef68d96550b4b31bc93bdb3
justin-0 <itsjustinoconnell@gmail.com>
2024-09-28 13:23:23 +0100

    react auto save, auto save added to editor to work with patch api endpoint

--- components/editor.tsx
+++ components/editor.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { Entry } from "@prisma/client";
+import { useAutosave } from "react-autosave";
+import { updateEntry } from "@/lib/api";
 
 type EditorProps = {
   data: Entry;
@@ -9,12 +11,23 @@ type EditorProps = {
 
 export default function Editor({ data }: EditorProps) {
   const [content, setContent] = React.useState(data.content);
+  const [isLoading, setIsLoading] = React.useState(false);
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
   };
 
+  useAutosave({
+    data: content,
+    onSave: async (value) => {
+      setIsLoading(true);
+      const updated = await updateEntry(data.id, value);
+      setIsLoading(false);
+    },
+  });
+
   return (
     <div className="w-full h-[calc(100vh-64px)] p-10">
+      {isLoading ? <span>Saving..</span> : null}
       <div>
         <textarea
           className="p-10 w-full h-[calc(100vh-120px)] text-xl outline-none"
