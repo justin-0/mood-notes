db152cb848d0be48bd7058c42e7eccc9e9a6ba22
justin-0 <itsjustinoconnell@gmail.com>
2024-09-28 11:26:54 +0100

    editor component

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { Entry } from "@prisma/client";
 
 type EditorProps = {
@@ -7,5 +8,20 @@ type EditorProps = {
 };
 
 export default function Editor({ data }: EditorProps) {
-  return <div>{data.content}</div>;
+  const [content, setContent] = React.useState(data.content);
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  return (
+    <div className="w-full h-[calc(100vh-64px)] p-10">
+      <div>
+        <textarea
+          className="p-10 w-full h-[calc(100vh-120px)] outline outline-black/5 text-xl"
+          value={content}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
 }
